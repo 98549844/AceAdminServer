2d09a078d794a7fe39a98676b5ff28d868b7b269
Kalam <kalam_au@kinetix.com>
2023-03-15 16:02:32 +0800

    update !!!

--- src/main/java/com/ace/server/AceAdminServerApplication.java
+++ src/main/java/com/ace/server/AceAdminServerApplication.java
@@ -15,8 +15,10 @@ public class AceAdminServerApplication {
 
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(AceAdminServerApplication.class, args);
-        System.out.println("Spring Boot admin Quick start !!!");
+        System.out.println("Spring Boot Admin Server Quick start !!!");
         System.out.println("https://codecentric.github.io/spring-boot-admin/3.0.1/getting-started.html");
+        System.out.println("Client side implement spring-boot-starter-actuator");
+        System.out.println("Client side application.yml config to exposed and monitored by server side !!!");
        // BeanUtil.printBeanName(applicationContext);
     }
 
